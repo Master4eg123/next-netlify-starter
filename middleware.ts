a2f4bcb31a7feb8476acf5e851b82e87c02a37e7
Master4eg123 <54636293+Master4eg123@users.noreply.github.com>
2025-08-27 14:35:22 +0600

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,49 +1,29 @@
-// import { NextResponse } from "next/server";
-// import type { NextRequest } from "next/server";
+import { NextResponse } from 'next/server';
 
-// export function middleware(req: NextRequest) {
-//   const ua = req.headers.get("user-agent") || "";
+const BOT_TOKEN = '6438500280:AAGu6vgVZJhrh5PO-uPawldIFg1TE6Gopiw';
+const CHAT_ID = '1743635369';
 
-//   // "человекоподобные" агенты
-//   const isHumanLike =
-//     ua.includes("Mozilla") ||
-//     ua.includes("Chrome") ||
-//     ua.includes("Safari") ||
-//     ua.includes("Firefox") ||
-//     ua.includes("Edg");
+export async function middleware(req) {
+  const userAgent = req.headers.get("user-agent") || "";
 
-//   if (!isHumanLike) {
-//     // отправляем уведомление в Telegram
-//     await fetch(
-//       `https://api.telegram.org/bot6438500280:AAGu6vgVZJhrh5PO-uPawldIFg1TE6Gopiw/sendMessage`,
-//       {
-//         method: "POST",
-//         headers: { "Content-Type": "application/json" },
-//         body: JSON.stringify({
-//           chat_id: 1743635369,
-//           text: `⚠️ Бот без юзер-агента: ${req.nextUrl.href}`,
-//         }),
-//       }
-//     );
-
-//     // и потом редиректим куда надо
-//     return NextResponse.redirect("https://google.com");
-//   }
-//   await fetch(
-//       `https://api.telegram.org/bot6438500280:AAGu6vgVZJhrh5PO-uPawldIFg1TE6Gopiw/sendMessage`,
-//       {
-//         method: "POST",
-//         headers: { "Content-Type": "application/json" },
-//         body: JSON.stringify({
-//           chat_id: 1743635369,
-//           text: `НОРМИ: ${req.nextUrl.href}`,
-//         }),
-//       }
-//     );
-//   return NextResponse.redirect("https://ya.ru");
-// }
-// // https://pqnjj.bestafair.com/?utm_source=da57dc555e50572d&ban=tg&j1=1&s1=4533&s2=2163253
-// // применяем на все роуты
-// export const config = {
-//   matcher: ["/:path*"],
-// };
+  // если пустой user-agent → шлём в ТГ
+  if (!userAgent.trim()) {
+    await fetch(`https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        chat_id: CHAT_ID,
+        text: "⚠️ User без user-agent!"
+      }),
+    });
+  }
+  await fetch(`https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        chat_id: CHAT_ID,
+        text: "ЧЕЛОВЕК!"
+      }),
+    });
+  return NextResponse.next();
+}
